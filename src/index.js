43d8b3d34d021e5e91bb254dec131822ef81412a
chr1swill <alexanderwilliams20003@gmail.com>
2023-12-07 08:43:35 -0500

    feat - testing config  more complex

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 console.log('Hello, World!');
 document.body.style.backgroundColor = 'red';
+document.body.innerHTML = `
+<h1>Hello, World!</h1>
+<h2>Hello, World!</h2>
+<h3>Hello, World!</h3>
+<h4>Hello, World!</h4>
+<h5>Hello, World!</h5>
+<h6>Hello, World!</h6>
+<img src='./asset/logo/logo.svg' alt='logo'>
+`;
