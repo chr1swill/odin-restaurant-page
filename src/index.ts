3e0a5af776f4b2f558ae99d4fb057050c109a61f
chr1swill <alexanderwilliams20003@gmail.com>
2023-12-06 21:35:19 -0500

    bug - tried to get it to work with tailwind, ts, and dev server will stick to the basic and make that work

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import myImage from './asset/menu-option/veggies/veggies.png';
+
+console.log('Hello, world!');
+
+const img = document.getElementById('my-image') as HTMLImageElement
+img.src = myImage;
