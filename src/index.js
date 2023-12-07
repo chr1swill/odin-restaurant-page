f069518e6d4778de3a71f12a16123fb1bfe8580c
chr1swill <alexanderwilliams20003@gmail.com>
2023-12-06 21:47:14 -0500

    feat - convert over to using only vanilla js, css, html

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import myImage from './asset/menu-option/veggies/veggies.png';
+
+console.log('Hello, world!');
+
+/**
+ * @type {HTMLImageElement} - img
+ */
+const img = document.getElementById('my-image')
+img.src = myImage
--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import myImage from './asset/menu-option/veggies/veggies.png';
-
-console.log('Hello, world!');
-
-const img = document.getElementById('my-image') as HTMLImageElement
-img.src = myImage;
