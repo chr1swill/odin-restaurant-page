58da1e598e9c3ca1c8c6e3ab9bfda549abe62a13
chr1swill <alexanderwilliams20003@gmail.com>
2023-12-07 08:29:44 -0500

    feat - created webpack.config.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import myImage from './asset/menu-option/veggies/veggies.png';
-
-console.log('Hello, world!');
-
-/**
- * @type {HTMLImageElement} - img
- */
-const img = document.getElementById('my-image')
-img.src = myImage
