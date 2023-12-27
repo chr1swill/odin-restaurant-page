84cfdb430fd19965982752db31dae542315edc31
chr1swill <alexanderwilliams20003@gmail.com>
2023-12-27 16:14:26 -0500

    refactor - dist/* to src/ before adding buid plugins

---
+++ src/app.js
@@ -0,0 +1,12 @@
+console.log('Hello, World!');
+document.body.style.backgroundColor = 'red';
+const img = './asset/menu-option/greek-salad/greek-salad.png';
+document.body.innerHTML = `
+<h1>Hello, World!</h1>
+<h2>Hello, World!</h2>
+<h3>Hello, World!</h3>
+<h4>Hello, World!</h4>
+<h5>Hello, World!</h5>
+<h6>Hello, World!</h6>
+<img src='${img}' alt='logo'>
+`;
