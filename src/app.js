ac25f87ed69f71396e7d9f6579774665266b5d93
chr1swill <alexanderwilliams20003@gmail.com>
2023-12-28 16:57:01 -0500

    feat - inline svg into my html - rm it from app.js

--- src/app.js
+++ src/app.js
@@ -2,26 +2,12 @@ import './asset/logo/logo.svg'
 import './component/Card'
 import './asset/menu-option/veggies/veggies.png'
 
-document.addEventListener('DOMContentLoaded', (event) => {
+const entryPoint = document.querySelector('#content')
 
-    let svgContent;
-
-    fetch('./asset/logo.svg')
-        .then(response => response.text())
-        .then(data => {
-            svgContent = data;
-
-            let header = document.querySelector('header');
-            let svgElement = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-            svgElement.innerHTML = svgContent;
-            header.appendChild(svgElement);
-
-            const entryPoint = document.querySelector('#content')
-
-            if(!entryPoint) {
-                console.error('Element not found')
-            } else {
-                entryPoint.innerHTML = `
+if(!entryPoint) {
+    console.error('Element not found')
+} else {
+    entryPoint.innerHTML = `
                  <card-component
                     title="Hello, World!"
                     text="Hello, World!"
@@ -31,6 +17,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
                  >
                  </card-component>
               `
-            }
-        });
-});
+}
