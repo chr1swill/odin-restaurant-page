1c1480a29587299dae12dd4385f1856e173ec04a
chr1swill <alexanderwilliams20003@gmail.com>
2023-12-28 09:17:44 -0500

    fix - config to server dist/ - contentBase is not valid change to static

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,37 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-   mode: 'development',
-   entry: './src/app.js',
-   output: {
-       filename: './dist/main.js',
-       path: path.resolve(__dirname, 'dist'),
-   },
-   module: {
-       rules: [
-           {
-               test: /\.css$/,
-               use: ['style-loader', 'css-loader']
-           },
-           {
-               test: /\.(png|webp|svg|jpg|gif)$/,
-               use: ['file-loader']
-           },
-           {
-               test: /\.html$/,
-               use: ['html-loader']
-           },
-       ]
-   },
-   plugins: [
-       new HtmlWebpackPlugin({
-           template: './src/index.html',
-           filename: 'index.html'
-       })
-   ],
-   devServer: {
-       contentBase: path.join(__dirname, 'dist'),
-       compress: true,
-       port: 9000
-   }
+    mode: 'development',
+    entry: './src/app.js',
+    output: {
+        filename: './dist/main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|webp|svg|jpg|gif)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    }
 };
