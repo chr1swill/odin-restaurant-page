c057d6d2442adfb09f20a87f5040836c969a7a67
chr1swill <alexanderwilliams20003@gmail.com>
2023-12-28 09:09:12 -0500

    fix - build error used wrong patch in index.html - use wrong path in config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
    mode: 'development',
    entry: './src/app.js',
    output: {
-       filename: 'main.js',
+       filename: './dist/main.js',
        path: path.resolve(__dirname, 'dist'),
    },
    module: {
