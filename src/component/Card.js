69f9bc818fd4aa3a34da92acca649f9f8b1063dd
chr1swill <alexanderwilliams20003@gmail.com>
2023-12-28 10:36:58 -0500

    feat - change component to get it to work - make the element in constructor - make change to error in style format so it will be visible

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,7 +1,47 @@
 class Card extends HTMLElement {
     constructor() {
         super();
-        this.shadowRoot = this.attachShadow({ mode: 'open' });
+        this.attachShadow({ mode: 'open' });
+        this.shadowRoot.innerHTML = `
+        <style>
+        section {
+        --padding: 20px;
+        --img-br: 10px;
+        width: 100%;
+        height: 100%;
+        padding: var(--padding);
+        display: grid;
+        place-content: center;
+        border-radius: calc(var(--img-br) + var(--padding));
+        }
+
+        img {
+        width: 100%;
+        height: auto;
+        aspect-ratio: 16/9;
+        border-radius: var(--img-br);
+        }
+
+        h2, p {
+        color: white;
+        }
+
+        h2 {
+        font-weight: bold;
+        font-size: 21px;
+        }
+
+        p {
+        font-weight: medium;
+        font-size: 16px;
+        }
+        </style>
+        <section data-background>
+        <img data-img>
+        <h2 data-title></h2>
+        <p data-text></p>
+        </section>
+        `
     }
 
     static get observedAttributes() {
@@ -38,40 +78,36 @@ class Card extends HTMLElement {
         }
     }
 
-    connectedCallback() {
-        this.render();
-    }
-
     updateImgSrc(newValue) {
-        const img = this.shadowRoot.querySelector('data-img')
+        const img = this.shadowRoot.querySelector('[data-img]')
         if(img) {
             img.src = newValue
         }
     }
 
     updateImgAlt(newValue) {
-        const img = this.shadowRoot.querySelector('data-img')
+        const img = this.shadowRoot.querySelector('[data-img]')
         if(img) {
             img.alt = newValue
         }
     }
 
     updateTitle(newValue) {
-        const h2 = this.shadowRoot.querySelector('data-title')
+        const h2 = this.shadowRoot.querySelector('[data-title]')
         if(h2) {
             h2.textContent = newValue 
         } 
     }
 
     updateText(newValue) {
-        const p = this.shadowRoot.querySelector('data-text')
+        const p = this.shadowRoot.querySelector('[data-text]')
         if(p) {
             p.textContent = newValue
         }
     }
 
     updateBackground(newValue) {
-        const bg = this.shadowRoot.querySelector('data-background')
+        const bg = this.shadowRoot.querySelector('[data-background]')
         if(bg) {
             bg.style.backgroundColor = newValue
         }
@@ -80,22 +116,22 @@ class Card extends HTMLElement {
     handleNullAttributes(name) {
         switch(name) {
             case 'imgsrc':
-                const img = this.shadowRoot.querySelector('data-img')
+                const img = this.shadowRoot.querySelector('[data-img]')
                 img.remove()
                 break
             case 'imgalt': 
                 img.alt = 'an image of something very nice'
                 break
             case 'title':
-                const h2 = this.shadowRoot.querySelector('data-title')
+                const h2 = this.shadowRoot.querySelector('[data-title]')
                 h2.remove()
                 break
             case 'text':
-                const p = this.shadowRoot.querySelector('data-text')
+                const p = this.shadowRoot.querySelector('[data-text]')
                 p.remove()
                 break
             case 'background':
-                const parent = this.shadowRoot.querySelector('data-background');
+                const parent = this.shadowRoot.querySelector('[data-background]');
 
                 while (parent.firstChild) {
                     parent.parentNode.insertBefore(parent.firstChild, parent);
@@ -107,45 +143,6 @@ class Card extends HTMLElement {
                 console.error(`${name} is not a valid attribute`)
 
         }
-    }
-
-    render() {
-        this.shadowRoot.innerText = `
-            <style>
-                section {
-                    --padding: 20px;
-                    --img-br: 10px;
-                    width: 100%;
-                    height: 100%;
-                    padding: var(--padding);
-                    display: grid;
-                    place-content: center;
-                    border-radius: calc(var(--img-br) + var(--padding);
-                }
-
-                img {
-                    width: 100%;
-                    height: auto;
-                    aspect-ratio: 16/9;
-                    border-radius: var(--img-br);
-                }
-
-                h2 {
-                    font-weight: bold;
-                    font-size: 21px;
-                }
-
-                p {
-                    font-weight: medium;
-                    font-size: 16px;
-                }
-            </style>
-            <section data-background>
-                <img data-img>
-                <h2 data-title></h2>
-                <p data-text></p>
-            </section>
-        `
     }
 }
 
