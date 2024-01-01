5360cc60c71ab49361a41c8ea91a26d050455d02
chr1swill <alexanderwilliams20003@gmail.com>
2024-01-01 06:21:51 -0500

    feat - move new card component to Card.js - import it in scr/view/home.js

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,148 +1,21 @@
-class Card extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = `
-        <style>
-        section {
-        --padding: 20px;
-        --img-br: 10px;
-        max-width: 100%;
-        height: auto;
-        padding: var(--padding);
-        display: grid;
-        place-content: center;
-        border-radius: calc(var(--img-br) + var(--padding));
-        }
-
-        img {
-        width: 100%;
-        height: auto;
-        border-radius: var(--img-br);
-        }
-
-        h2, p {
-        color: white;
-        }
-
-        h2 {
-        font-weight: bold;
-        font-size: 21px;
-        }
-
-        p {
-        font-weight: medium;
-        font-size: 16px;
-        }
-        </style>
-        <section data-background>
-        <img data-img>
-        <h2 data-title></h2>
-        <p data-text></p>
-        </section>
-        `
-    }
-
-    static get observedAttributes() {
-        return ['imgsrc', 'imgalt', 'title', 'text', 'background']
-    }
-
-    attributeChangedCallback(name, oldValue, newValue) {
-        if(newValue === null) {
-            this.handleNullAttributes(name)
-            return
-        } else if(oldValue === newValue) {
-            return
-        }
-
-        switch(name) {
-            case 'imgsrc':
-                this.updateImgSrc(newValue)
-                break
-            case 'imgalt':
-                this.updateImgAlt(newValue)
-                break
-            case 'title':
-                this.updateTitle(newValue)
-                break
-            case 'text':
-                this.updateText(newValue)
-                break
-            case 'background':
-                this.updateBackground(newValue)
-                break
-            default: 
-                console.error(`${name} is not a valid attribute`)
-
-        }
-    }
-
-    updateImgSrc(newValue) {
-        const img = this.shadowRoot.querySelector('[data-img]')
-        if(img) {
-            img.src = newValue
-        }
-    }
-
-    updateImgAlt(newValue) {
-        const img = this.shadowRoot.querySelector('[data-img]')
-        if(img) {
-            img.alt = newValue
-        }
-    }
-
-    updateTitle(newValue) {
-        const h2 = this.shadowRoot.querySelector('[data-title]')
-        if(h2) {
-            h2.textContent = newValue 
-        } 
-    }
-
-    updateText(newValue) {
-        const p = this.shadowRoot.querySelector('[data-text]')
-        if(p) {
-            p.textContent = newValue
-        }
-    }
-
-    updateBackground(newValue) {
-        const bg = this.shadowRoot.querySelector('[data-background]')
-        if(bg) {
-            bg.style.backgroundColor = newValue
-        }
-    }
-
-    handleNullAttributes(name) {
-        switch(name) {
-            case 'imgsrc':
-                const img = this.shadowRoot.querySelector('[data-img]')
-                img.remove()
-                break
-            case 'imgalt': 
-                img.alt = 'an image of something very nice'
-                break
-            case 'title':
-                const h2 = this.shadowRoot.querySelector('[data-title]')
-                h2.remove()
-                break
-            case 'text':
-                const p = this.shadowRoot.querySelector('[data-text]')
-                p.remove()
-                break
-            case 'background':
-                const parent = this.shadowRoot.querySelector('[data-background]');
-
-                while (parent.firstChild) {
-                    parent.parentNode.insertBefore(parent.firstChild, parent);
-                }
-
-                parent.remove();
-                break
-            default: 
-                console.error(`${name} is not a valid attribute`)
-
-        }
-    }
+/**
+ * creates a cardComponent
+ *
+ * @param {string} imgSrc - the path or url to your image
+ * @param {string} imgAlt - the alt text for your image
+ * @param {string} title - the content of the h2 in your card
+ * @param {string} description - the content of the p in your card
+ * @returns {string} - a cardComponent
+ */
+export const cardComponent = (imgSrc, imgAlt, title, description) => {
+    return `
+            <section data-id="card">
+                <img data-img
+                    src="${imgSrc}"
+                    alt="${imgAlt}"
+                >
+                <h2 data-title>${title}</h2>
+                <p data-text>${description}</p>
+            </section>
+    ` 
 }
-
-customElements.define('card-component', Card);
--- src/view/home.js
+++ src/view/home.js
@@ -1,28 +1,6 @@
-import '../component/Card'
+import { cardComponent } from '../component/Card'
 import '../asset/characters/charles.png'
 import '../asset/characters/darryl.png'
-
-/**
- * creates a cardComponent
- *
- * @param {string} imgSrc - the path or url to your image
- * @param {string} imgAlt - the alt text for your image
- * @param {string} title - the content of the h2 in your card
- * @param {string} description - the content of the p in your card
- * @returns {string} - a cardComponent
- */
-const cardComponent = (imgSrc, imgAlt, title, description) => {
-    return `
-            <section data-id="card">
-                <img data-img
-                    src="${imgSrc}"
-                    alt="${imgAlt}"
-                >
-                <h2 data-title>${title}</h2>
-                <p data-text>${description}</p>
-            </section>
-    ` 
-}
 
 export const homePage = () => {
     return `
